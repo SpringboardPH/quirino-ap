8ff2164c3e358a65507a81f7417e72f6f5d0d822
Jay Termulo <jetermulo@gmail.com>
2024-09-04 13:40:04 +0800

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./build/*.html"],
+  theme: {
+    extend: {
+      // color: {
+      //   primary: "#02A9F7",
+      //   foreground: "#01303F",
+      //   darkBlue: "#02577A",
+      //   lightBlue: "#89D6FB",
+      //   background: "#D4F0FC",
+      //   secondary: "#DE5147"
+      // },
+    },
+  },
+  plugins: [],
+}
